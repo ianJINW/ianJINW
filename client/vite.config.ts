8568254761da7b563d976417247be1f6c5eb1b59
Njenga Ian <joshnjenga066@gmail.com>
2026-09-10 17:46:08 +0300

    Fisrt step - Set up

---
+++ client/vite.config.ts
@@ -0,0 +1,13 @@
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ], server: {
+    port: 5173
+  }
+})
